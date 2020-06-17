147488084c2f71ea1160857b88327c8366ae6458
haothing <crosstime.sun@gmail.com>
2020-06-17 17:32:11 +0900

    update train source

--- crnn/lib/core/function.py
+++ crnn/lib/core/function.py
@@ -114,7 +114,7 @@ def validate(config, val_loader, dataset, converter, model, criterion, device, e
 
     raw_preds = converter.decode(preds.data, preds_size.data, raw=True)[:config.TEST.NUM_TEST_DISP]
     for raw_pred, pred, gt in zip(raw_preds, sim_preds, labels):
-        print('{:50}\t[{}  <>  {}]'.format(raw_pred, pred, gt))
+        print('{:80}        [ {}   <>   {} ]'.format(raw_pred, pred, gt))
 
     accuracy = n_correct / float(config.TEST.NUM_TEST * config.TEST.BATCH_SIZE_PER_GPU)
     print('correct: {}/{} \ttest loss: {:.4f} \taccuray: {:.4f}'.format(n_correct, config.TEST.NUM_TEST * config.TEST.BATCH_SIZE_PER_GPU, losses.avg, accuracy))
--- crnn/train.py
+++ crnn/train.py
@@ -173,5 +173,5 @@ def main():
         cleanup_dist()
 
 if __name__ == '__main__':
-    for i in range(50):
+    for i in range(1):
         main()
